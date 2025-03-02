d3233630ffb9830b63d37a68495b345b72398c17
anamsilva1981 <ana.msilva@hotmail.com>
2025-03-02 10:42:21 -0300

    Add reactive login form with validation to HomeComponent

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,15 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   public loginCard = true;
+
+  public loginForm = this.formBuilder.group({
+    email: this.formBuilder.control('', Validators.required),
+    password: this.formBuilder.control('', Validators.required),
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  onSubmitLoginForm(): void{
+    console.log('Dados do formulario de Login enviados com sucess', this.loginForm.value)
+  }
 }
