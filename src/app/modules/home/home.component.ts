06bcd522a48f9c8eaabe81f76cbf91051777eb74
anamsilva1981 <ana.msilva@hotmail.com>
2025-03-02 10:47:13 -0300

    Add reactive signup form with validation and submission handling in HomeComponent

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -15,9 +15,19 @@ export class HomeComponent {
     password: this.formBuilder.control('', Validators.required),
   })
 
+  public signupForm = this.formBuilder.group({
+    name: this.formBuilder.control('', Validators.required),
+    email: this.formBuilder.control('', Validators.required),
+    password: this.formBuilder.control('', Validators.required),
+  })
+
   constructor(private formBuilder: FormBuilder) { }
 
-  onSubmitLoginForm(): void{
+  public onSubmitLoginForm(): void{
     console.log('Dados do formulario de Login enviados com sucess', this.loginForm.value)
+  }
+
+  public onSubmitSignupForm(): void{
+    console.log('Dados do formulario de Cadastro enviados com sucess', this.signupForm.value)
   }
 }
